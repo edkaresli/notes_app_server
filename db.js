398f0ec90b2111788e4955e87416a66b6c58e61f
Eduard Karesli <ed.karesli@gmail.com>
2019-11-04 00:03:39 +0100

    Some additions

--- db.js
+++ db.js
@@ -1,24 +1,39 @@
 let sqlite3 = require('sqlite3').verbose();
 
-let db = new sqlite3.Database('./DB/NotesApp.db', (err) => {
+let db = new sqlite3.Database('./DB/NotesApp.db', sqlite3.OPEN_READWRITE, (err) => {
     if(err) {
         console.log(err.message);
         // return err;
     }
 
-    console.log('Connected to notes.txt database.');
+    console.log('Connected to ./DB/NotesApp.db database.');
 });
 
 db.serialize( () => {
   
-  db.run("CREATE TABLE notes (note_id INTEGER PRIMARY KEY, note_title TEXT, note_body TEXT)");
+  db.run("CREATE TABLE [IF NOT EXISTS] notes (note_id INTEGER PRIMARY KEY, note_title TEXT, note_body TEXT)");
   
   // Create and then insert into the language table the text translations for the interface 
-  db.run("CREATE TABLE language (lang_id TEXT, interface TEXT)");
-
+  db.run("CREATE TABLE [IF NOT EXISTS] language (lang_id TEXT, interface TEXT)");
   
 });
 
+const find = (id) => {
+  db.run('SELECT * FROM notes WHERE note_id = ' + id, (err, result) => {
+    console.log(result);
+  });
+}
 
+const insertNote = (id, title, body) => {
+  db.run('INSERT INTO notes (?, ?, ?)', id, title, body);
+}
+
+const update = (id, title, body) => {
+   
+}
+
+const localize = (lang_id) => {
+  // Get the localization JSON object from the language table by the lang_id
+}
 
 module.exports = db;
